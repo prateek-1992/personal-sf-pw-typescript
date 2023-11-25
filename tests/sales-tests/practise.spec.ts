e4ab16c7ea3f3a8e7af3a213091a2dd430ec712b
Prateek Parashar <prateek.parashar@quillbot.com>
2023-11-26 02:34:51 +0530

    testing pipeline for env variables

--- tests/sales-tests/practise.spec.ts
+++ tests/sales-tests/practise.spec.ts
@@ -4,7 +4,7 @@ test.describe(`Debugging demo/practise`, async () => {
   test(`@Demo TC001 - verfying navigation from pw home page using get started`, async ({
     page,
   }) => {
-    console.log(`Environment coming from CI is ${process.env['priority']}`);
+    console.log(`Environment coming from CI is ${process.env['PRIORITY']}`);
     await page.goto('https://playwright.dev/');
     await page.getByText('Get Started').click();
     await expect(page).toHaveURL('https://playwright.dev/docs/intro');
