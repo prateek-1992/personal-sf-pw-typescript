53d043449e4ec052fcbd60f287572c5b63ee7bfa
Prateek Parashar <prateek.parashar@quillbot.com>
2023-11-26 02:18:12 +0530

    adding a demo script

--- tests/sales-tests/practise.spec.ts
+++ tests/sales-tests/practise.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from '@playwright/test';
 
 test.describe(`Debugging demo/practise`, async () => {
-  test(`TC001 - verfying navigation from pw home page using get started`, async ({ page }) => {
+  test(`@Demo TC001 - verfying navigation from pw home page using get started`, async ({
+    page,
+  }) => {
     await page.goto('https://playwright.dev/');
     await page.getByText('Get Started').click();
     await expect(page).toHaveURL('https://playwright.dev/docs/intro');
