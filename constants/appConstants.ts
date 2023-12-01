bd99695ed84089ad3f7bc94d69e101a34c3a2959
Prateek Parashar <prateek.parashar@quillbot.com>
2023-12-02 01:14:15 +0530

    adding base variable to .env files

--- constants/appConstants.ts
+++ constants/appConstants.ts
@@ -1,7 +1,7 @@
 export enum AppConstants {
-  INSTANCE_URL = 'https://qeagle-dev-ed.lightning.force.com/lightning/setup/SetupOneHome/home',
-  SETUP_HOMEPAGE_URL = 'https://qeagle-dev-ed.lightning.force.com/lightning/setup/SetupOneHome/home',
-  SALES_HOMEPAGE_URL = 'https://qeagle-dev-ed.lightning.force.com/lightning/page/home',
-  ACCOUNT_PAGE = 'https://qeagle-dev-ed.lightning.force.com/lightning/o/Account/list?filterName=Recent',
-  NEW_ACCOUNT_FORM_PAGE = 'https://qeagle-dev-ed.lightning.force.com/lightning/o/Account/new',
+  INSTANCE_URL = '/lightning/setup/SetupOneHome/home',
+  SETUP_HOMEPAGE_URL = '/lightning/setup/SetupOneHome/home',
+  SALES_HOMEPAGE_URL = '/lightning/page/home',
+  ACCOUNT_PAGE = '/lightning/o/Account/list?filterName=Recent',
+  NEW_ACCOUNT_FORM_PAGE = '/lightning/o/Account/new',
 }
